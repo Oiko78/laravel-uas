3ccf79d52a737f8129a422e02fc1757f6ac2eaf7
Oliver Chico <oliverchiko2002@gmail.com>
2023-02-03 20:16:20 +0700

    sementara

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn () => redirect(route('items.index')));
+Route::get('/success', fn () => view('shared.success'));
+
+Route::controller(UserController::class)->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::prefix('/register')->group(function () {
+            Route::get('', 'create')->name('register');
+            Route::post('', 'store')->name('users.create');
+        });
+        Route::prefix('/login')->group(function () {
+            Route::get('', 'login')->name('login');
+            Route::post('', 'authenticate')->name('users.authenticate');
+        });
+    });
+    Route::middleware('auth')->group(function () {
+        Route::prefix('/profile')->group(function () {
+            Route::get('', 'show')->name('users.show');
+            Route::post('', 'update')->name('users.update');
+        });
+
+        Route::post('/logout', 'logout')->name('logout');
+    });
 });
+
+Route::controller(CartController::class)
+    ->prefix('cart')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('', 'index')->name('cart.index');
+        Route::post('', 'update')->name('cart.update');
+        Route::delete('', 'remove')->name('cart.remove');
+        Route::post('checkout', 'destroy')->name('cart.checkout');
+    });
+
+Route::controller(ItemController::class)
+    ->middleware('auth')
+    ->prefix('items')
+    ->group(function () {
+        Route::get('', 'index')->name('items.index');
+        Route::get('{item}', 'show')->name('items.show');
+    });
+
+Route::controller(UserController::class)
+    ->middleware('auth')
+    // ->middleware('admin')
+    ->group(function () {
+        Route::get('/users', 'index')->name('users.index');
+    });
